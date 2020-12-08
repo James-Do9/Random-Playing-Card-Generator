290ca6d003b903f92c750c6b8c0cd174d4ef30bc
James-Do9 <dojames3@yahoo.com>
2020-12-08 19:20:08 +0000

    Intial commit

--- src/main.js
+++ src/main.js
@@ -5,6 +5,43 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  let topSuit = document.querySelector(".topSuit");
+  let bottomSuit = document.querySelector(".bottomSuit");
+  let img = document.querySelector(".suitIcon");
+  let num = generateRandomNumber();
+  let suit = generateRandomSuit();
+  img.innerHTML = suit;
+  topSuit.innerHTML = num + suit;
+  bottomSuit.innerHTML = num + suit;
+  if (suit == "&#9829" || suit == "&#9830") {
+    topSuit.style.color = "red";
+    img.style.color = "red";
+    bottomSuit.style.color = "red";
+  }
 };
+function generateRandomNumber() {
+  let numArray = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let index = Math.floor(Math.random() * numArray.length);
+  return numArray[index];
+}
+
+function generateRandomSuit() {
+  let numArray = ["&#9824", "&#9827", "&#9829", "&#9830"];
+  let index = Math.floor(Math.random() * numArray.length);
+  return numArray[index];
+}
